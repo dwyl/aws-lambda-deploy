1b0e42a2b73e76808f43378fb0e76748ee99ba33
nelsonic <contact.nelsonic+github@gmail.com>
2016-02-23 09:26:11 +0000

    export essential methods as lib/index.js defined as "main" in package.json

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('./test/00_env.test.js');
-require('./lib/copy_files')();
-require('./lib/install_node_modules')();
-require('./lib/zip')();
-var upload = require('./lib/upload');
-upload(function (err, data) {
+var dpl = require('./lib');
+dpl.copy_files();
+dpl.install_node_modules();
+dpl.zip();
+dpl.upload(function (err, data) {
   console.log('Err:', err);
   console.log('Lambda Function:');
   console.log(data);
