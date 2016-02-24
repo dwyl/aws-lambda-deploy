fe537c3803a305b9c6e2a72f7021b51109f754f7
nelsonic <contact.nelsonic+github@gmail.com>
2016-02-24 13:25:25 +0000

    add regression test for: https://github.com/numo-labs/aws-lambda-deploy/issues/21

--- test/02_copy_files.test.js
+++ test/02_copy_files.test.js
@@ -24,6 +24,15 @@ describe('copy_files', function () {
     done();
   });
 
+  // Regression test for: https://github.com/numo-labs/aws-lambda-deploy/issues/21
+  it('confirm nested directory is correctly copied over', function (done) {
+    copy_files();
+    var file_path = process.env.TMPDIR + 'dist/lib/schema/index.js';
+    var exists = fs.statSync(file_path);
+    assert(exists);
+    done();
+  });
+
   it('delete /dist directory for next test', function (done) {
     var dist_path = process.env.TMPDIR + 'dist';
     var exists = false;
