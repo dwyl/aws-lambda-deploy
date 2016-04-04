b1c0c371cb31818314e3a4dfa04ce820f1e0c933
nelsonic <contact.nelsonic+github@gmail.com>
2016-04-04 20:11:06 +0100

    remove excess console.log from 04_zip.test.js

--- test/04_zip.test.js
+++ test/04_zip.test.js
@@ -11,13 +11,9 @@ var zip = require('../lib/zip');
 
 describe('zip', function () {
   it('zip the /dist directory', function (done) {
-    // console.log('helllo');
     copy_files(); // setup /dist
-    // console.log('copy_files ...')
-    // require('./dont_install_babel_in_prod.js')(); // don't install 200mb of Babel!!
     install_node_modules();
     var zip_file_path = path.normalize(process.env.TMPDIR + pkg.name + '.zip');
-    // console.log('>> zip_file_path', zip_file_path)
     zip();
     var stat = fs.statSync(zip_file_path);
     assert(stat.size > 1000000); // the zip is bigger than a megabyte!
@@ -27,7 +23,6 @@ describe('zip', function () {
   it(' unzip the package and confirm the package.json is intact', function (done) {
     zip.unzip();
     var unzipped = path.normalize(process.env.TMPDIR + 'unzipped');
-    // console.log('unzipped:', unzipped);
     var unzipped_utils = require(path.normalize(unzipped + '/lib/utils'));
     assert.equal(JSON.stringify(utils), JSON.stringify(unzipped_utils));
     utils.delete_dir_contents(unzipped, true);   // delete unzipped completely
