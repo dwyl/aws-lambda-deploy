2ba11c47d814d427fa4de8db473f3ed33baed27f
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-16 11:59:13 +0100

    minor re-factor of lib/upload.js to accomodate passing in a custom package.json (simplest way to test changing settings) and allows us to have a multi-lambda project in the future...

--- test/06_lambda_settings.test.js
+++ test/06_lambda_settings.test.js
@@ -1,64 +1,70 @@
-
 'use strict';
+require('env2')('.env');
 var fs = require('fs');
 var assert = require('assert');
-var decache = require('decache');
+var copy_files = require('../lib/copy_files');
+var install_node_modules = require('../lib/install_node_modules');
+var zip = require('../lib/zip');
+var upload = require('../lib/upload');
 var utils = require('../lib/utils');
+
 var base_path = utils.get_base_path()
 var AWS = require('aws-sdk');
 AWS.config.region = process.env.AWS_REGION; // set your Environment Variables...
 var lambda = new AWS.Lambda();
 var FUNCTION_NAME; // GLOBAL used to delete the function.
-var PKG_FROZEN = JSON.parse(JSON.stringify(require(base_path + 'package.json')));
+var pkg = require(base_path + 'package.json');
 
 describe('upload > testing lambda_timeout and lambda_memory', function () {
-  before('Setup the package.json to have the optional settings', function (done) {
-    var pkg = JSON.parse(JSON.stringify(PKG_FROZEN));
-    pkg.lambda_memory = '512'; // 512 MB
-    pkg.lambda_timeout = '42'; // 42 seconds
-    // delete pkg.lambda_memory;
-    // delete pkg.lambda_timeout;
-    fs.writeFileSync(base_path + 'package.json', JSON.stringify(pkg, null, 2));
-    done();
+  it('exercise default values for lambda_memory & timeout', function (done) {
+    delete pkg.lambda_memory;
+    delete pkg.lambda_timeout;
+    copy_files();
+    install_node_modules();
+    zip();
+    // pass in the modified pkg (with lambda_memory & lambda_timeout set)
+    upload(pkg, function (err, data) {
+      assert(!err);
+      console.log('Lambda Function CREATED:', data);
+      FUNCTION_NAME = data.FunctionName;
+
+      assert(data.CodeSize > 100000);
+      // these are the default values:
+      assert.equal(data.MemorySize, 128);
+      assert.equal(data.Timeout, 10);
+      // now delete the function so we can do it all again!
+      lambda.deleteFunction({ FunctionName: FUNCTION_NAME }, function (err, data) {
+        assert.equal(err, null);
+        done();
+      });
+    });
   });
 
-  it('upload the lambda function to S3', function (done) {
-    decache('../lib/upload');
-    decache(base_path + 'package.json');
-    decache('../lib/utils');
-    decache('../lib/copy_files');
-
-    var copy_files = require('../lib/copy_files');
-    var install_node_modules = require('../lib/install_node_modules');
-    var zip = require('../lib/zip');
-    var utils = require('../lib/utils');
-    var upload = require('../lib/upload');
+  it('Max the settings for lambda_memory & timeout', function (done) {
+    pkg.lambda_memory = 1536; // 1.5 GB (the most you can have!)
+    pkg.lambda_timeout = 300; // 300 seconds (max execution time)
 
     copy_files();
     install_node_modules();
     zip();
-    upload(function (err, data) {
+    // pass in the modified pkg (with lambda_memory & lambda_timeout set)
+    upload(pkg, function (err, data) {
       assert(!err);
       console.log('Lambda Function CREATED:', data);
-      FUNCTION_NAME = data.FunctionName;
+
       assert(data.CodeSize > 100000);
-      // assert.equal(data.Timeout, 42);
-      // assert.equal(data.MemorySize, 512);
-      done();
+      // these are the default values:
+      assert.equal(data.MemorySize, 1536);
+      assert.equal(data.Timeout, 300);
+      // delete it again!
+      lambda.deleteFunction({ FunctionName: FUNCTION_NAME }, function (err, data) {
+        assert.equal(err, null);
+        done();
+      });
     });
   });
 
-  it('DELETE the Lambda Function from AWS so its clean', function (done) {
-    lambda.deleteFunction({ FunctionName: FUNCTION_NAME }, function (err, data) {
-      assert.equal(err, null);
-      // restore the package.json to original state:
-      fs.writeFileSync(base_path + 'package.json', JSON.stringify(PKG_FROZEN, null, 2));
-      done();
-    });
-  });
-
-  after('clean_up > DELETE the /dist folder and lambda.zip', function (done) {
-    var pkg = require(base_path + 'package.json');
+  after('clean_up > restore package.json, DELETE the /dist folder and lambda.zip', function (done) {
     utils.clean_up();
     var file_path = process.env.TMPDIR + pkg.name + '.zip';
     var exists = false;
